5d2a1708a1cc93b1a2f6eff850de6e08419d8f31
Andrew Kennedy <andrewk36@gmail.com>
2016-10-12 00:47:49 -0700

    more changes

--- .config/fish/path.fish
+++ .config/fish/path.fish
@@ -13,6 +13,7 @@
 # a great way to set $PATH.
 # A good resource is https://nothingbutsnark.svbtle.com/trying-out-the-fish-shell
 set --export --global ANDROID_TOOLS $HOME/android-sdk/platform-tools
+set --export --global LOCAL_BIN $HOME/.local/bin
 
 # These lines were for a manual npm install to avoid needing sudo
 # ========================================================
@@ -22,3 +23,5 @@ set --export --global ANDROID_TOOLS $HOME/android-sdk/platform-tools
 # set fish_user_paths $NPM_PACKAGES/bin $fish_user_paths
 
 set fish_user_paths $ANDROID_TOOLS $fish_user_paths
+set fish_user_paths $LOCAL_BIN $fish_user_paths
+
